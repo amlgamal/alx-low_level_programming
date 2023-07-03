b35c8c79164f885a785199a540b7e6c19650dfa0
lia <lia_hhh@hohoh.com>
2023-07-03 13:04:44 -0700

    a

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Enty point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Alawys 0 (Success)
+*/
+
+
+int main(void)
+{
+        Puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+} 
